e369935b6e415ada491e994e693ce42c3217c8e2
Daniel <deco2449584@gmail.com>
2023-07-03 15:46:17 -0500

    se realiza diseño de el header y el sidebar identico al de figma, utilizando los svg de ese proyecto

--- SKG/clon_almeria/index.js
+++ SKG/clon_almeria/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 mapboxgl.accessToken =
   "pk.eyJ1IjoiaW5nanVhbm1hc3VhcmV6IiwiYSI6ImNsZDZjMXJpYTFhdzgzdnBhZXdkczQxcnQifQ.25y1PWrOTW12YssZ73JQtA";
 
@@ -62,68 +61,3 @@ mapa.on("load", () => {
     });
   });
 });
-=======
-mapboxgl.accessToken =
-  "pk.eyJ1IjoiaW5nanVhbm1hc3VhcmV6IiwiYSI6ImNsZDZjMXJpYTFhdzgzdnBhZXdkczQxcnQifQ.25y1PWrOTW12YssZ73JQtA";
-
-const mapa = new mapboxgl.Map({
-  container: "contenedor-del-mapa",
-  style: "mapbox://styles/mapbox/dark-v10",
-  center: [-74.12, 4.65],
-  zoom: 10,
-  /* attributionControl: false, // Deshabilita el control de atribución */
-});
-mapa.addControl(new mapboxgl.NavigationControl());
-
-mapa.on("load", () => {
-  mapa.loadImage("./img/map-bus-stop.png", function (error, image) {
-    if (error) throw error;
-
-    mapa.addImage("custom-icon", image);
-
-    mapa.addSource("puntosDeInteres", {
-      type: "geojson",
-      data: {
-        type: "FeatureCollection",
-        features: [
-          {
-            type: "Feature",
-            properties: {},
-            geometry: {
-              coordinates: [-74.10966159424332, 4.691148644459972],
-              type: "Point",
-            },
-          },
-          {
-            type: "Feature",
-            properties: {},
-            geometry: {
-              coordinates: [-74.10966159424332, 4.59602699923056],
-              type: "Point",
-            },
-          },
-          {
-            type: "Feature",
-            properties: {},
-            geometry: {
-              coordinates: [-74.03024860019572, 4.739745959795826],
-              type: "Point",
-            },
-          },
-        ],
-      },
-    });
-
-    mapa.addLayer({
-      id: "puntosDeInteres",
-      type: "symbol",
-      source: "puntosDeInteres",
-      layout: {
-        "icon-image": "custom-icon",
-        "icon-size": 0.5,
-        "icon-allow-overlap": true,
-      },
-    });
-  });
-});
->>>>>>> cb05dbdd33a42da23777a911500815823d888435
--- SKG/mapbox/src/app/components/map/map.component.ts
+++ SKG/mapbox/src/app/components/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { environment } from '../../environments/environment';
 import { DataService } from '../../data/data.service';
@@ -8,34 +8,45 @@ import { DataService } from '../../data/data.service';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
   constructor(private dataService: DataService) {}
-  ngOnInit() {
+
+  ngAfterViewInit() {
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/dark-v10',
       center: [-74.12, 4.65],
       zoom: 9,
-      accessToken: environment.mapboxToken,
+      attributionControl: false,
+      accessToken: (mapboxgl as any).accessToken || environment.mapboxToken,
     });
-    this.dataService.getDatosFromAPI().subscribe((data: any) => {
-      // Procesar los datos recibidos y agregarlos al mapa
-      if (data && data.features) {
-        map.addSource('datos', {
-          type: 'geojson',
-          data: data,
-        });
 
-        map.addLayer({
-          id: 'datos-layer',
-          type: 'circle',
-          source: 'datos',
-          paint: {
-            'circle-radius': 6,
-            'circle-color': 'blue',
-          },
-        });
-      }
+    map.on('style.load', () => {
+      map.addControl(new mapboxgl.NavigationControl());
+
+      this.dataService.getDatosFromAPI().subscribe((data: any) => {
+        // Procesar los datos recibidos y agregarlos al mapa
+        if (data && data.features) {
+          map.addSource('datos', {
+            type: 'geojson',
+            data: data,
+          });
+
+          map.addLayer({
+            id: 'datos-layer',
+            type: 'circle',
+            source: 'datos',
+            paint: {
+              'circle-radius': 6,
+              'circle-color': 'blue',
+            },
+          });
+        }
+      });
     });
+  }
+
+  ngOnInit() {
+    // Código de inicialización adicional si es necesario
   }
 }
